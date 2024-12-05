74d09da834e5f9064437ba45a7d87594424f0c92
Deepanshu Mehra <deepanshumehra2102@gmail.com>
2024-12-05 23:13:33 +0530

    feat : completed 2 more exercise

--- 01-exercise/01-goroutines/03-join/main.go
+++ 01-exercise/01-goroutines/03-join/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
@@ -10,12 +11,16 @@ func main() {
 	// deterministically.
 
 	var data int
+	var wg sync.WaitGroup
+	wg.Add(1)
 
 	go func() {
+		defer wg.Done()
+
 		data++
 	}()
+	wg.Wait()
 
 	fmt.Printf("the value of data is %v\n", data)
-
 	fmt.Println("Done..")
 }
